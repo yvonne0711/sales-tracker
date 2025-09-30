5a11967e3673c7f118cf44fa1cab69c344950fff
Beth <bmfclarke@gmail.com>
2025-09-30 12:06:53 +0100

    rds terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+variable "AWS_REGION" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type      = string
+  sensitive = true
+}
+
+variable "VPC_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "SUBNET_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "VPC_CIDR" {
+  type      = string
+  sensitive = true
+}
+
+variable "DB_NAME" {
+  type      = string
+  sensitive = true
+}
+
+variable "DB_USERNAME" {
+  type      = string
+  sensitive = true
+}
+
+variable "DB_PASSWORD" {
+  type      = string
+  sensitive = true
+}
