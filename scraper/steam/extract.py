b79bdfcd2778387cda0934bdcd7f36c574e2be75
TristenX <1604tristenarron@gmail.com>
2025-09-30 12:29:01 +0100

    Gets db connection

--- scraper/steam/extract.py
+++ scraper/steam/extract.py
@@ -3,8 +3,24 @@ Script that gets Steam product details from the RDS and scrapes their
 prices from their respective URLs.
 """
 
+from os import environ
+
 import requests as req
 from bs4 import BeautifulSoup
+from psycopg2 import connect
+from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+
+def get_db_connection() -> connection:
+    """Returns a live connection from the database."""
+    return connect(user=environ["DB_USERNAME"],
+                   password=environ["DB_PASSWORD"],
+                   host=environ["DB_IP"],
+                   port=environ["DB_PORT"],
+                   database=environ["DB_NAME"],
+                   cursor_factory=RealDictCursor)
 
 
 def is_discounted(url: str, discounted_class: str, headers: dict[str:str]) -> bool:
@@ -36,23 +52,12 @@ def get_current_price(product_details: dict[str:str], headers: dict[str:str]) ->
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     user_agent = {
         "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, \
             like Gecko) Chrome/140.0.0.0 Safari/537.36"
     }
 
-    steam_product_discounted = {
-        "url": "https://store.steampowered.com/app/3314790/CloverPit/",
-        "price_class": "game_purchase_price price",
-        "discount_class": "discount_final_price"
-    }
-
-    steam_product = {
-        "url": "https://store.steampowered.com/app/2486820/Sonic_Racing_\
-            CrossWorlds/?snr=1_4_4__118",
-        "price_class": "game_purchase_price price",
-        "discount_class": "discount_final_price"
-    }
-
-    print(get_current_price(steam_product, user_agent))
+    db_conn = get_db_connection()
