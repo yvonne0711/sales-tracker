8ea13bde79069b5f4592393f06e66653979f715a
yvonne <yvonnewong1107@gmail.com>
2025-10-01 18:37:37 +0100

    add iam role for eventbridge

--- terraform/phase_two/main.tf
+++ terraform/phase_two/main.tf
@@ -1,63 +1,85 @@
 #lambdas, ECS, eventbridge
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
 
 # Lambdas 
-resource "aws_iam_role" "c19-sales-tracker-lambda-execution-role" {
-  name = "c19-sales-tracker-lambda-execution-role"
+# resource "aws_iam_role" "c19-sales-tracker-lambda-execution-role" {
+#   name = "c19-sales-tracker-lambda-execution-role"
 
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#       },
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "c19-sales-tracker-lambda-basic-execution" {
+#   role       = aws_iam_role.c19-sales-tracker-lambda-execution-role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
+
+# resource "aws_lambda_function" "c19-sales-tracker-lambda-steam" {
+#   function_name = "c19-sales-tracker-lambda-steam"
+#   role          = aws_iam_role.c19-sales-tracker-lambda-execution-role.arn
+#   package_type  = "Image"
+#   #   image_uri     = ""
+#   memory_size   = 512
+#   timeout       = 30
+#   architectures = ["x86_64"]
+
+#   environment {
+#     variables = {
+#       DB_HOST     = var.DB_HOST
+#       DB_PORT     = var.DB_PORT
+#       DB_NAME     = var.DB_NAME
+#       DB_USERNAME = var.DB_USERNAME
+#       DB_PASSWORD = var.DB_PASSWORD
+#     }
+#   }
+# }
+
+# resource "aws_lambda_function" "c19-sales-tracker-lambda-email" {
+#   function_name = "c19-sales-tracker-lambda-email"
+#   role          = aws_iam_role.c19-sales-tracker-lambda-execution-role.arn
+#   package_type  = "Image"
+#   #   image_uri     = ""
+#   memory_size   = 512
+#   timeout       = 30
+#   architectures = ["x86_64"]
+
+#   environment {
+#     variables = {
+#       DB_HOST     = var.DB_HOST
+#       DB_PORT     = var.DB_PORT
+#       DB_NAME     = var.DB_NAME
+#       DB_USERNAME = var.DB_USERNAME
+#       DB_PASSWORD = var.DB_PASSWORD
+#     }
+#   }
+# }
+
+# eventbridge iam role 
+resource "aws_iam_role" "c19-sales-tracker-scheduler-role" {
+  name = "c19-sales-tracker-scheduler-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "scheduler.amazonaws.com"
+        }
       }
-      },
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "c19-sales-tracker-lambda-basic-execution" {
-  role       = aws_iam_role.c19-sales-tracker-lambda-execution-role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "c19-sales-tracker-lambda-steam" {
-  function_name = "c19-sales-tracker-lambda-steam"
-  role          = aws_iam_role.c19-sales-tracker-lambda-execution-role.arn
-  package_type  = "Image"
-  #   image_uri     = ""
-  memory_size   = 512
-  timeout       = 30
-  architectures = ["x86_64"]
-
-  environment {
-    variables = {
-      DB_HOST     = var.DB_HOST
-      DB_PORT     = var.DB_PORT
-      DB_NAME     = var.DB_NAME
-      DB_USERNAME = var.DB_USERNAME
-      DB_PASSWORD = var.DB_PASSWORD
-    }
-  }
-}
-
-resource "aws_lambda_function" "c19-sales-tracker-lambda-email" {
-  function_name = "c19-sales-tracker-lambda-email"
-  role          = aws_iam_role.c19-sales-tracker-lambda-execution-role.arn
-  package_type  = "Image"
-  #   image_uri     = ""
-  memory_size   = 512
-  timeout       = 30
-  architectures = ["x86_64"]
-
-  environment {
-    variables = {
-      DB_HOST     = var.DB_HOST
-      DB_PORT     = var.DB_PORT
-      DB_NAME     = var.DB_NAME
-      DB_USERNAME = var.DB_USERNAME
-      DB_PASSWORD = var.DB_PASSWORD
-    }
-  }
 }
