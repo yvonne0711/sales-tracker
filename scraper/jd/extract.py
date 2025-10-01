292353b2b28834e7d5a2910e0f00673d427bd824
Yacquub <b_yacquub@hotmail.co.uk>
2025-10-01 09:13:30 +0100

    edited extract for jd

---
+++ scraper/jd/extract.py
@@ -0,0 +1,89 @@
+"""
+Script that gets Steam product details from the RDS and scrapes their
+prices from their respective URLs.
+"""
+
+from os import environ
+
+import requests as req
+from bs4 import BeautifulSoup
+from psycopg2 import connect
+from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+
+def get_db_connection() -> connection:
+    """Returns a live connection from the database."""
+    return connect(user=environ["DB_USERNAME"],
+                   password=environ["DB_PASSWORD"],
+                   host=environ["DB_HOST"],
+                   port=environ["DB_PORT"],
+                   database=environ["DB_NAME"],
+                   cursor_factory=RealDictCursor)
+
+
+def query_database(conn: connection, sql: str) -> list[dict]:
+    """Returns the result of a query to the database."""
+    with conn.cursor() as cursor:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    return result
+
+
+def get_name(url: str, name_class: str, headers: dict[str:str]) -> str:
+    """Checks if the product itemprop class is present on the webpage to get the name."""
+    res = req.get(url, headers=headers, timeout=5)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, "html.parser")
+        if soup.find(attrs={"itemprop": name_class}) is not None:
+            name = soup.find(attrs={"itemprop": name_class}).text.strip()
+            return name
+        return False
+    return res.status_code, res.reason
+
+
+def is_discounted(url: str, discounted_class: str, headers: dict[str:str]) -> bool:
+    """Checks if the product price_class is present on the webpage."""
+    res = req.get(url, headers=headers, timeout=5)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, "html.parser")
+        if soup.find(attrs={"class": discounted_class}) is not None:
+            return True
+        return False
+    return res.status_code, res.reason
+
+
+def scrape_price(url: str, cost_class: str, headers: dict[str:str]) -> str:
+    """Returns the price of a product for the product URL and cost class."""
+    res = req.get(url, headers=headers, timeout=5)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.text, "html.parser")
+        price = soup.find(attrs={"class": cost_class}).text.strip()
+        return price
+    return res.status_code, res.reason
+
+
+def get_current_price(url: str, cost_class: str, discounted_class: str, name_class, headers: dict[str:str]) -> str:
+    """Returns the current price of a product from its details."""
+    name_product = get_name(url, name_class, headers)
+    if is_discounted(url, discounted_class, headers):
+        return name_product, scrape_price(url, discounted_class, headers)
+    return name_product, scrape_price(url, cost_class, headers)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    user_agent = {
+        "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, \
+            like Gecko) Chrome/140.0.0.0 Safari/537.36"
+    }
+
+    jd_cost_class = "pri"
+    jd_discounted_class = "now"
+    jd_name_class = 'name'
+
+    db_conn = get_db_connection()
+    db_conn.close()
