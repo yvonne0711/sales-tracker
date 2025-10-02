39efb83f568c6100f8cb37a1e63cf795187d6d52
TristenX <1604tristenarron@gmail.com>
2025-10-02 20:31:02 +0100

    Removed redundant functions

--- scraper/steam/load.py
+++ scraper/steam/load.py
@@ -8,8 +8,7 @@ from psycopg2.extensions import connection
 
 from extract import (get_db_connection,
                      get_products,
-                     get_last_recorded_prices,
-                     query_database)
+                     get_last_recorded_prices)
 from transform import format_products
 
 
@@ -26,28 +25,6 @@ def update_price(conn: connection, product: dict) -> None:
                                product["price"],
                                product["check_at"]))
         conn.commit()
-
-
-def get_steam_subscribers(conn: connection) -> list[dict]:
-    """Returns a list of subscribers to steam products."""
-    query = """
-    SELECT user_name,
-    user_email,
-    desired_price,
-    product_name,
-    product_url,
-    product_id
-    FROM users
-    JOIN subscription
-    USING(user_id)
-    JOIN product
-    USING(product_id)
-    JOIN website
-    USING(website_id)
-    WHERE website_name = 'steam';
-    """
-    result = query_database(conn, query)
-    return result
 
 
 def compare_prices(conn: connection, products: list[dict]) -> dict[int:float]:
@@ -67,27 +44,6 @@ def compare_prices(conn: connection, products: list[dict]) -> dict[int:float]:
     return updated
 
 
-def check_price_against_required_price(subs: list[dict],
-                                       updated_items: dict[int: float]) -> list[dict]:
-    """
-    Returns a list of user/product information that will be used to generate an
-    email if the price is bellow their desired price.
-    """
-    email_info = []
-    for sub in subs:
-        if sub["product_id"] in updated_items:
-            if sub["desired_price"] >= updated_items[sub["product_id"]]:
-                info = {}
-                info["user_name"] = sub["user_name"]
-                info["user_email"] = sub["user_email"]
-                info["desired_price"] = float(sub["desired_price"])
-                info["product_name"] = sub["product_name"]
-                info["product_url"] = sub["product_url"]
-                info["current_price"] = updated_items[sub["product_id"]]
-                email_info.append(info)
-    return email_info
-
-
 def handler(event=None, context=None) -> dict[str:str]:
     """Handler function for Lambda that uploads price changes to the RDS."""
     user_agent = {
@@ -105,11 +61,11 @@ def handler(event=None, context=None) -> dict[str:str]:
                                      steam_discounted_class,
                                      user_agent,
                                      last_recorded_prices)
-    compare_prices(db_conn, steam_products)
+    updated_prices = compare_prices(db_conn, steam_products)
     db_conn.close()
 
     return {
-        "message": "Success"
+        "price_updates": updated_prices
     }
 
 
