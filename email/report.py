52b5a1c3dfc5c6b70eeba837e60252f37825fd50
yvonne <yvonnewong1107@gmail.com>
2025-10-02 10:30:12 +0100

    remove write html file

--- email/report.py
+++ email/report.py
@@ -4,46 +4,35 @@ from datetime import datetime
 from dotenv import load_dotenv
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
+import boto3
 
 
-
-def generate_html_report(data, file, mode=True):
-    """Generates an HTML report from the data."""
+def generate_html_report(row):
+    """Generates an HTML report for a row of user and product."""
     today = datetime.now().date()
 
-    # take data from each row
-    for row in data:
-        user_email = row["user_email"]
-        desired_price = row["desired_price"]
-        new_price = row["new_price"]
-        user_name = row["user_name"]
-        product_name = row["product_name"]
-        url = row["url"]
+    user_email = row["user_email"]
+    desired_price = row["desired_price"]
+    new_price = row["new_price"]
+    user_name = row["user_name"]
+    product_name = row["product_name"]
+    url = row["url"]
 
-        html = f"""
-        <html>
-        <head>
-            <title>New sale! - {today}</title>
-        </head>
-        <body>
-            <h2>Hi {user_name},</h2>
-            <p>Good news! The product {product_name} you're subscribed to is on sale.</p>
-            <h3><a href='{url}'>{product_name}</a></h3>
-            <p><b>£{desired_price} -> £{new_price}</b></p>
-            <p>Thank you.</p>
-        </body>
-        </html>
-        """
-
-    if mode is True:
-        with open(file, mode="w", encoding="utf-8") as f:
-            f.write(html)
-    return {
-        "statusCode": 200,
-        "headers": {"Content-Type": "text/html"},
-        "body": html
-    }
-
+    html = f"""
+    <html>
+    <head>
+        <title>New sale! - {today}</title>
+    </head>
+    <body>
+        <h2>Hi {user_name},</h2>
+        <p>Good news! The product {product_name} you're subscribed to is on sale.</p>
+        <h3><a href='{url}'>{product_name}</a></h3>
+        <p><b>£{desired_price} -> £{new_price}</b></p>
+        <p>Thank you.</p>
+    </body>
+    </html>
+    """
+    return html
 
 def handler(event=None, context=None):
     """Handler for Lambda."""
