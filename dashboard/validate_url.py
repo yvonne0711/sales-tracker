bf1af259bf952ccf402b56cc4fcd9003d632b9ee
TristenX <1604tristenarron@gmail.com>
2025-10-03 11:59:33 +0100

    Gets website name and starts to validate steam

--- dashboard/validate_url.py
+++ dashboard/validate_url.py
@@ -2,6 +2,7 @@
 
 import requests as req
 from dotenv import load_dotenv
+from psycopg2.extensions import connection
 
 from functions_dashboard import get_db_connection
 
@@ -17,6 +18,27 @@ def check_response(url: str, headers: dict[str:str]) -> bool:
         return False
 
 
+def get_website_name(conn: connection, website_id: int) -> str:
+    """Returns the result of a query to the database."""
+    with conn.cursor() as cursor:
+        query = """
+        SELECT website_name
+        FROM website
+        WHERE website_id = %(website_id)s;
+        """
+        cursor.execute(query, {"website_id": website_id})
+        result = cursor.fetchone()
+    return result["website_name"]
+
+
+def validate_steam(url: str, headers: dict[str:str]) -> bool:
+    """Checks that the price classes steam uses are present."""
+    res = req.get(url, timeout=5, headers=headers)
+    if res.status_code == 200:
+        pass
+    return False
+
+
 if __name__ == "__main__":
     load_dotenv()
 
@@ -27,3 +49,9 @@ if __name__ == "__main__":
     }
 
     db_conn = get_db_connection()
+
+    website_id = 1
+    product_name = "Test"
+    product_url = "https://store.steampowered.com/app/990080/Hogwarts_Legacy/"
+
+    website_name = get_website_name(db_conn, 1)
