6b2ed3dda39c845f4db620b5a4b30a9ea734d8a2
TristenX <1604tristenarron@gmail.com>
2025-10-03 12:07:29 +0100

    Validates steam urls

--- dashboard/validate_url.py
+++ dashboard/validate_url.py
@@ -1,6 +1,7 @@
 """Contains functions that validate a URL."""
 
 import requests as req
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from psycopg2.extensions import connection
 
@@ -33,9 +34,14 @@ def get_website_name(conn: connection, website_id: int) -> str:
 
 def validate_steam(url: str, headers: dict[str:str]) -> bool:
     """Checks that the price classes steam uses are present."""
+    cost_class = "game_purchase_price price"
+    discounted_class = "discount_final_price"
     res = req.get(url, timeout=5, headers=headers)
     if res.status_code == 200:
-        pass
+        soup = BeautifulSoup(res.text, "html.parser")
+        if soup.find(attrs={"class": discounted_class}) is not None or soup.find(attrs={"class": discounted_class}):
+            return True
+        return False
     return False
 
 
@@ -52,6 +58,15 @@ if __name__ == "__main__":
 
     website_id = 1
     product_name = "Test"
-    product_url = "https://store.steampowered.com/app/990080/Hogwarts_Legacy/"
+    product_url = "https://www.next.co.uk/style/su079917/d49871"
 
-    website_name = get_website_name(db_conn, 1)
+    if check_response(product_url, user_agent):
+        print("yes")
+        website_name = get_website_name(db_conn, 1)
+        if website_name == "steam":
+            if validate_steam(product_url, user_agent):
+                print("yes")
+            else:
+                print("no")
+    else:
+        print("no")
