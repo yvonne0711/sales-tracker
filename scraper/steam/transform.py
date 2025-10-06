64c4f9200a3804234d5f495a41fccf1df7357750
TristenX <1604tristenarron@gmail.com>
2025-10-06 11:37:49 +0100

    Removed redundant code block/imports

--- scraper/steam/transform.py
+++ scraper/steam/transform.py
@@ -4,12 +4,7 @@ Script that transforms the product details into the appropriate datatypes.
 
 from datetime import datetime
 
-from dotenv import load_dotenv
-
-from extract import (get_db_connection,
-                     get_products,
-                     get_current_price,
-                     get_last_recorded_prices)
+from extract import get_current_price
 
 
 def convert_string_price_to_float(price: str) -> float:
@@ -53,28 +48,3 @@ def format_products(products: dict[str:str], cost_class: str,
             product["db_price"] = price_map[product["product_id"]]
         product["check_at"] = datetime.now()
     return products
-
-
-if __name__ == "__main__":
-    load_dotenv()
-
-    user_agent = {
-        "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, \
-            like Gecko) Chrome/140.0.0.0 Safari/537.36"
-    }
-
-    steam_cost_class = "game_purchase_price price"
-    steam_discounted_class = "discount_final_price"
-
-    db_conn = get_db_connection()
-
-    steam_products = get_products(db_conn)
-    last_recorded_prices = get_last_recorded_prices(db_conn)
-    steam_products = format_products(steam_products,
-                                     steam_cost_class,
-                                     steam_discounted_class,
-                                     user_agent,
-                                     last_recorded_prices)
-
-    db_conn.close()
