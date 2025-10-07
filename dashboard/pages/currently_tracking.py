97ff7c41de8752876ebee097eb07bb9927c39108
yvonne <yvonnewong1107@gmail.com>
2025-10-07 09:49:24 +0100

    add check to see is user is logged in

--- dashboard/pages/currently_tracking.py
+++ dashboard/pages/currently_tracking.py
@@ -13,3 +13,24 @@ def delete_subscription(conn, subscription_id: int) -> None:
         cur.execute(query, (subscription_id,))
         conn.commit()
 
+
+def main():
+    """Initialise the currently tracking page."""
+    # Check if user is logged in first
+    if 'logged_in' not in st.session_state or not st.session_state.logged_in:
+        st.error("Please log in to access this page")
+        if st.button("Go to Login Page"):
+            st.switch_page("login")
+        st.stop()
+
+    st.set_page_config(page_title="Souper Saver", layout="wide")
+    st.header("My Currently Tracked Products")
+
+    user = st.session_state.user
+    conn = get_db_connection()
+
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    main()
