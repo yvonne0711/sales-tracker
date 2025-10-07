dfd31284940880f891001ed2f9d9ac8e2f45df93
TristenX <1604tristenarron@gmail.com>
2025-10-07 15:38:11 +0100

    Changed scrape_price to be consistent

--- scraper/steam/extract.py
+++ scraper/steam/extract.py
@@ -4,6 +4,7 @@ prices from their respective URLs.
 """
 
 from os import environ
+import re
 
 import requests as req
 from bs4 import BeautifulSoup
@@ -73,42 +74,17 @@ def get_html_text(url: str, headers: dict[str:str]) -> tuple[int, str]:
     return res.status_code, res.reason
 
 
-def is_discounted(html: tuple[int, str], discounted_class: str) -> bool:
-    """Checks if the product price_class is present on the webpage."""
-    soup = BeautifulSoup(html[1], "html.parser")
-    if soup.find(attrs={"class": discounted_class}) is not None:
-        return True
-    return False
-
-
-def scrape_price(html: tuple[int, str], cost_class: str) -> str:
+def scrape_price(html: tuple[int, str], container_class: str) -> str:
     """Returns the price of a product for the product URL and cost class."""
     soup = BeautifulSoup(html[1], "html.parser")
-    price = soup.find(attrs={"class": cost_class}).text.strip()
-    if price:
-        return price
+    price_container = soup.find(attrs={"class": container_class})
+    divs = price_container.find_all("div")
+    return re.search(r"£\d+(?:\.\d{2})?", divs[-2].text.strip()).group()
 
 
-def get_current_price(url: str, cost_class: str, discounted_class: str, headers: dict[str:str]) -> str:
+def get_current_price(url: str, container_class: str, headers: dict[str:str]) -> str:
     """Returns the current price of a product from its details."""
     html_text = get_html_text(url, headers)
     if html_text[0] == 200:
-        if is_discounted(html_text, discounted_class):
-            return scrape_price(html_text, discounted_class)
-        return scrape_price(html_text, cost_class)
+        return scrape_price(html_text, container_class)
     return html_text
-
-
-if __name__ == "__main__":
-    user_agent = {
-        "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, \
-        like Gecko) Chrome/140.0.0.0 Safari/537.36"
-    }
-    steam_cost_class = "game_purchase_price price"
-    steam_discounted_class = "discount_final_price"
-    steam_xpath = '//*[@id="game_area_purchase_section_add_to_cart_415372"]/div[2]/div/div[1]/text()'
-    html_text = get_html_text(
-        "https://store.steampowered.com/app/881100/Noita/",
-        user_agent)
-    print(get_current_price(html_text, steam_xpath))
--- scraper/steam/test_extract.py
+++ scraper/steam/test_extract.py
@@ -3,40 +3,69 @@
 from unittest.mock import patch
 
 from extract import (scrape_price,
-                     is_discounted,
                      get_current_price)
+
+
+HTML = """    <div class="game_area_purchase_game">
+        <div class="game_area_purchase_platform">
+            <span class="platform_img win"></span>
+        </div>
+
+        <div class="game_purchase_action">
+            <div class="game_purchase_action_bg">
+                <div class="game_purchase_price price" data-price-final="4999">
+                    £49.99
+                </div>
+                <div class="btn_addtocart">
+                    <a class="btn_green_steamui btn_medium"
+                    data-panel='{"focusable":true,"clickOnActivate":true}'
+                    href="javascript:addToCart(822363);"
+                    id="btn_add_to_cart_822363"
+                    role="button">
+                        <span>Add to Cart</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="game_purchase_action_bg">
+            <div class="game_purchase_price price" data-price-final="4999">
+                £49.99
+            </div>
+            <div class="btn_addtocart">
+                <a class="btn_green_steamui btn_medium"
+                data-panel='{"focusable":true,"clickOnActivate":true}'
+                href="javascript:addToCart(822363);"
+                id="btn_add_to_cart_822363"
+                role="button">
+                    <span>Add to Cart</span>
+                </a>
+            </div>
+        </div>
+
+        <div class="game_purchase_price price" data-price-final="4999">
+            £49.99
+        </div>
+
+        <div class="btn_addtocart">
+            <a class="btn_green_steamui btn_medium"
+            data-panel='{"focusable":true,"clickOnActivate":true}'
+            href="javascript:addToCart(822363);"
+            id="btn_add_to_cart_822363"
+            role="button">
+                <span>Add to Cart</span>
+            </a>
+        </div>
+
+        £49.99
+    </div>"""
 
 
 def test_scrape_price():
     """Tests that the scrape function returns the price."""
-    steam_cost_class = "game_purchase_price price"
-    html = (200,
-            "<div class='game_purchase_price price' data-price-final='5999'>£59.99</div>")
-    assert scrape_price(html, steam_cost_class) == '£59.99'
-
-
-def test_scrape_discounted_price():
-    """Tests that the is_discount function works with non none values."""
-    steam_discounted_class = "discount_final_price"
-    html = (200,
-            "<div class='discount_final_price'>£39.99</div>")
-    assert scrape_price(html, steam_discounted_class) == '£39.99'
-
-
-def test_is_discounted_true():
-    """Tests that the is_discount function works with none values."""
-    steam_discounted_class = "discount_final_price"
-    html = (200,
-            "<div class='discount_final_price'>£39.99</div>")
-    assert is_discounted(html, steam_discounted_class) == True
-
-
-def test_is_discounted_false():
-    """Tests that the is_discount function works with none values."""
-    steam_discounted_class = "discount_final_price"
-    html = (200,
-            "<div class='game_purchase_price price' data-price-final='5999'>£59.99</div>")
-    assert is_discounted(html, steam_discounted_class) == False
+    html_tuple = (200, HTML)
+    result = scrape_price(html_tuple, "game_area_purchase_game")
+    assert result == "£49.99"
 
 
 @patch("extract.get_html_text")
@@ -45,24 +74,7 @@ def test_get_current_price_false(mock_html):
     Tests that the functions work together to get the price of the product 
     if discount is false.
     """
-    mock_html.return_value = (200,
-                              "<div class='game_purchase_price price' data-price-final='5999'>£59.99</div>")
-    steam_cost_class = "game_purchase_price price"
-    steam_discounted_class = "discount_final_price"
-    result = '£59.99'
-
-    assert get_current_price("fake_http", steam_cost_class, steam_discounted_class, {
-                             "User-Agent": "test"}) == result
-
-
-@patch("extract.get_html_text")
-def test_get_current_price(mock_html):
-    """Tests that the functions work together to get the price of the product if discount is true."""
-    mock_html.return_value = (200,
-                              "<div class='discount_final_price'>£39.99</div>")
-    steam_cost_class = "game_purchase_price price"
-    steam_discounted_class = "discount_final_price"
-    result = '£39.99'
-
-    assert get_current_price("fake_http", steam_cost_class, steam_discounted_class, {
-                             "User-Agent": "test"}) == result
+    mock_html.return_value = (200, HTML)
+    result = get_current_price("fake_http", "game_area_purchase_game", {
+        "User-Agent": "test"})
+    assert result == "£49.99"
