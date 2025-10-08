c8f0716d50b2c306f3325ebc412eb9d56c181f5f
TristenX <1604tristenarron@gmail.com>
2025-10-08 17:05:04 +0100

    Abstracted get html text

--- dashboard/scraper_functions.py
+++ dashboard/scraper_functions.py
@@ -4,36 +4,38 @@ import requests as req
 from bs4 import BeautifulSoup
 
 
-def get_steam_product_name(url: str, headers: dict[str:str]) -> str:
-    """Returns the name of a steam product from a url."""
+def get_html_text(url: str, headers: dict[str:str]) -> str:
+    """Gets HTML text from a url."""
     res = req.get(url, headers=headers, timeout=5)
     if res.status_code == 200:
-        soup = BeautifulSoup(res.text, "html.parser")
-        product_name = soup.find(
-            attrs={"id": "appHubAppName"}).text.strip()
-        return product_name
+        return res.text
     return None
+
+
+def get_steam_product_name(url: str, headers: dict[str:str]) -> str:
+    """Returns the name of a steam product from a url."""
+    html_text = get_html_text(url, headers)
+    soup = BeautifulSoup(html_text, "html.parser")
+    product_name = soup.find(
+        attrs={"id": "appHubAppName"}).text.strip()
+    return product_name
 
 
 def get_next_product_name(url: str, headers: dict[str:str]) -> str:
     """Returns the name of a next product from a url."""
-    res = req.get(url, headers=headers, timeout=5)
-    if res.status_code == 200:
-        soup = BeautifulSoup(res.text, "html.parser")
-        product_name = soup.find(
-            attrs={"class": "pdp-css-1b3j8zg"}).text.strip()
-        return product_name
-    return None
+    html_text = get_html_text(url, headers)
+    soup = BeautifulSoup(html_text, "html.parser")
+    product_name = soup.find(
+        attrs={"class": "pdp-css-1b3j8zg"}).text.strip()
+    return product_name
 
 
 def get_jd_product_name(url: str, headers: dict[str:str]) -> str:
     """Returns the name of a jd product from a url."""
-    res = req.get(url, headers=headers, timeout=5)
-    if res.status_code == 200:
-        soup = BeautifulSoup(res.text, "html.parser")
-        product_name = soup.find(attrs={"itemprop": "name"}).text.strip()
-        return product_name
-    return None
+    html_text = get_html_text(url, headers)
+    soup = BeautifulSoup(html_text, "html.parser")
+    product_name = soup.find(attrs={"itemprop": "name"}).text.strip()
+    return product_name
 
 
 def get_product_name(website: str, url: str) -> str:
